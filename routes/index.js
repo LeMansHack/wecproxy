f28fb7666fab3656d4f6f65b3ca432bdf5defbfb
Jesper Blad Jensen <deldy@deldysoft.dk>
2014-06-14 13:36:09 +0200

    Track and cars is now separated

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,25 @@ router.get('/', function(req, res) {
   client = new Client();
 
   // direct way
-  client.get("http://live.fiawec.com/proxy.php?file=1/live/data.js", function(data, response){
+  client.get("http://live.fiawec.com/proxy.php?file=1/live/data.js&t=" + new Date().getTime(), function(data, response){
+
       // parsed response body as js object
-      res.json(JSON.parse(data));
+      var uglyData = JSON.parse(data);
+
+      // First object is global data
+      var globalData = uglyData[0];
+      var cars = [];
+      for(var i = 1; i< uglyData.length; i++) {
+        cars.push(uglyData[i]);
+      }
+
+      res.json({
+        track: globalData,
+        cars: cars
+      })
+
   });
+
 });
 
 module.exports = router;
