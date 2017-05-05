44bc66b23462864cd2bc5762fe7940bfc397a4de
Kasper Baagø Jensen <kasper@edentic.dk>
2017-05-05 12:01:56 +0200

    Updated WECProxy to new client format

--- routes/index.js
+++ routes/index.js
@@ -64,80 +64,92 @@ function getData(done) {
   client = new Client();
 
   // direct way
-  client.get("http://live.fiawec.com/wpphpFichiers/1/live/FIAWEC/data.js?t=" + ticks, function(data, response){
+  client.get("http://www.fiawec.com/ecm/live/WEC/data.json?_=" + ticks, function(data, response){
 
       // parsed response body as js object
-      var uglyData = JSON.parse(data);
-
-      // First object is global data
-      var globalData = uglyData[1];
-      var weatherData = globalData[11][0];
+      var params = JSON.parse(data.params);
+      var entries = JSON.parse(data.entries);
+      var driverResult = JSON.parse(data.driversResult);
+      //console.log(params);
+      //console.log(entries);
+      //console.log(driverResult);
       
       var track = {
-        localTime: moment(globalData[3], "HH:mm"),
-        flag: globalData[6],
-        safetyCar: globalData[9] == 1,
-        qualify: globalData[1] == 1,
-        elapsedTime: globalData[4],
-        remainingTime: globalData[7],
-        elapsedTimeInSeconds: parseTime(globalData[4]),
-        remainingTimeInSeconds: parseTime(globalData[7]),
-        notification: globalData[2],
-        logo: globalData[5],
+        localTime: parseInt(params.timestamp),
+        flag: params.racestate,
+        safetyCar: (params.safetycar === "true"),
+        qualify: null,
+        elapsedTime: null,
+        remainingTime: null,
+        elapsedTimeInSeconds: params.elapsedTime,
+        remainingTimeInSeconds: params.remaining,
+        notification: null,
+        logo: params.svg,
+        eventName: params.eventName,
         weather: {
-          airTemp: parseFloat(weatherData[3]),
-          roadTemp: parseFloat(weatherData[6]),
-          humidity: parseFloat(weatherData[2]),
-          airPreassure: parseFloat(weatherData[5]),
-          windSpeed: parseFloat(weatherData[8]),
-          windDirection: parseFloat(weatherData[0]),
-          weatherType: weatherData[1],
-          weatherIcon: weatherData[7],
-          weatherIconUrl: "http://live.fiawec.com/_include/wec/images/meteo/"+weatherData[7]+".png"
+          airTemp: parseFloat(params.airTemp),
+          roadTemp: parseFloat(params.trackTemp),
+          humidity: parseFloat(params.humidity),
+          airPreassure: parseFloat(params.pressure),
+          windSpeed: parseFloat(params.windSpeed),
+          windDirection: parseFloat(params.windDirection),
+          weatherType: params.weather,
+          weatherIcon: '',
+          weatherIconUrl: ''
         },
-        isDay: globalData[12]
+        isDay: null
       };
       var cars = [];
-      var carData = uglyData[0];
+      var carData = entries;
 
       var tmp = Object.keys(carData);
       for(var i = 0; i< tmp.length; i++) {
         var car = carData[tmp[i]];
 
-        var pilot = car[5] == '' ? null : staticData.tabPilotes[car[5]];
-        var extraData = getExtraData(car[2]);
+        //var pilot = car[5] == '' ? null : staticData.tabPilotes[car[5]];
+        //var extraData = getExtraData(car[2]);
 
         cars.push({
-          pilot: pilot == null ? null : {
-            lastName: pilot.prenom,
-            firstName: pilot.nom,
-            country: staticData.tabPays[pilot.pays].en,
-            birthday: pilot.birth,
-            picture: "http://live.fiawec.com/wpphpFichiers/1/pilote/"+car[5]+"/" + pilot.pic,
-            site: pilot.site,
-            facebook: pilot.facebook,
-            twitter: pilot.twitter
+          pilot: {
+            lastName: '',
+            firstName: car.driver,
+            country: '',
+            birthday: '',
+            picture: '',
+            site: '',
+            facebook: '',
+            twitter: ''
           },
-          driverStatus: car[9],
-          laps: parseInt(car[13]),
-          time: car[0],
-          timeDifference: car[4],
-          bestTimeInMiliseconds: parseLapTime(car[8]),
-          lastTimeInMiliseconds: parseLapTime(car[12]),
-          pits: parseInt(car[16]),
-          averageSpeed: parseFloat(car[1]),
-          tires: car[6],
-          wec: car[10],
-          d1l1: car[14],
-          d1l2: car[3],
-          d2l1: car[7],
-          d2l2: car[11],
-          avg: car[15],
-          team: extraData.team,
-          number: extraData.number,
-          category: extraData.category,
-          carBrand: extraData.carBrand,
-          carName: extraData.carName
+          driverStatus: car.state,
+          laps: parseInt(car.lap),
+          time: car.lastPassingTime,
+          timeDifference: car.gap,
+          bestTimeInMiliseconds: parseLapTime(car.bestlap),
+          lastTimeInMiliseconds: parseLapTime(car.lastlap),
+          pits: car.pitstop,
+          averageSpeed: parseFloat(car.speed),
+          tires: car.tyre,
+          wec: car.isWEC,
+          d1l1: car.d1l1,
+          d1l2: car.d2l1,
+          d2l1: null,
+          d2l2: null,
+          avg: car.av_time,
+          team: car.team,
+          number: car.number,
+          category: car.category,
+          carBrand: '',
+          carName: car.car,
+          position: car.position,
+          ranking: car.ranking,
+          categoryPosition: car.categoryPosition,
+          sector: car.sector,
+          currentSector1: car.currentSector1,
+          currentSector2: car.currentSector2,
+          currentSector3: car.currentSector3,
+          bestSector1: car.bestSector1,
+          bestSector2: car.bestSector2,
+          bestSector3: car.bestSector3
         });
       }
 
@@ -179,28 +191,6 @@ function parseLapTime(str) {
   }
 
   return parseInt(mil + sec*1000 + min*60*1000);
-}
-
-function getExtraData(id) {
-  if(id == null)
-    return {};
-
-  var engageData = staticData.tabEngages[id];
-  if(!engageData) {
-    return {}
-  }
-
-  var teamData = staticData.tabTeams[engageData.team];
-  var carData =  staticData.tabVehicules[engageData.voiture];
-  var brand = staticData.tabMarques[carData.marque];
-  var categoryData = staticData.tabCategories[engageData.categorie];
-  return {
-    team: teamData.nom,
-    number: engageData.num,
-    category: categoryData.nom,
-    carBrand: brand,
-    carName: carData.nom
-  }
 }
 
 module.exports = router;
